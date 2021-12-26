8e5a108351e0d708fceb1420710dafce62c2bd64
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-12-26 13:11:58 +0900

    Access to DB from PHP

--- provision/web/php.sh
+++ provision/web/php.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing PHP and setting it up..."
-# Install PHP 7.0 and module for apache2 to use PHP
-apt-get install -y php7.0 libapache2-mod-php7.0
+# Install PHP 7.0 and module for Apache2 and MySQL
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql
+
+# Restart Apache2
+systemctl restart apache2
