9dcc46f4225fdec0489febf976e00cc082dd470b
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2021-12-26 16:22:47 +0900

    simple app

--- provision/db/db_init.sql
+++ provision/db/db_init.sql
@@ -1,3 +1,17 @@
+# Register a user
 CREATE USER 'lamp'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'lamp'@'%';
 FLUSH PRIVILEGES;
+
+# Setting DB
+CREATE DATABASE IF NOT EXISTS lamp_db DEFAULT CHARACTER SET utf8;
+USE lamp_db;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    user_id INT(5) NOT NULL AUTO_INCREMENT,
+    user_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+INSERT INTO users (user_name) VALUES ('Alice');
--- src/index/index.php
+++ src/index/index.php
@@ -1,17 +1,57 @@
 <?php
 
-$mysqli = new mysqli('192.168.33.11', 'lamp', '', 'mysql');
-
-if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-    exit();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // When the request method is GET, no params are submitted.
+    $showPattern = 'form';
+    $msg = "Haven't we met before?<br/>Let me know your Name.";
 } else {
-    $mysqli->set_charset("utf8");
-    echo 'SUCESS to access to DB';
+    // When the request method is POST, the user_name is submitted.
+    // Make connetcion to DB server
+    $mysqli = new mysqli('192.168.33.11', 'lamp', '', 'lamp_db');
+
+    if ($mysqli->connect_error) {
+        // When DB returns error
+        $msg = "Sorry, I'm confused.<br/>Leave me alone...";
+    } else {
+        $userName = $_POST['user_name'];
+        $userName = $mysqli->real_escape_string($userName);
+        
+        if (getIsFirst($mysqli, $userName)) {
+            // When the user comes for the first time
+            $isSuccess = registerUser($mysqli, $userName);
+            if ($isSuccess) {
+                // When success to register user_name
+                $msg = "Oh, this is the first time to see you.<br/>Nice to meet you, {$userName}!";
+            } else {
+                // When failure to register user_name
+                $msg = "Excuse me?";
+            }
+        } else {
+            $msg = "Oh, Long time no see, {$userName}!";
+        }
+    }
+    $mysqli->close();
 }
 
-$mysqli->close();
+function getIsFirst(mysqli $mysqli, string $userName): bool
+{
+    $query = "SELECT * FROM users WHERE user_name = '" . $userName . "';";
+    $resultNum = $mysqli->query($query)->num_rows;
 
+    if ($resultNum === 0) {
+        $isFirst = true;
+    } else {
+        $isFirst = false;
+    }
+    return $isFirst;
+}
+
+function registerUser(mysqli $mysqli, string $userName): bool
+{
+    $query = "INSERT INTO users (user_name) VALUES ('" . $userName . "');";
+    $isSuccess = $mysqli->query($query);
+    return $isSuccess;
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +60,17 @@ $mysqli->close();
         <link href="./index.css" rel="stylesheet">
     </head>
     <body>
-        <h1 class="h1-index">Hello Vagrant!</h1>
+        <h1 class="h1-index">
+            <?php echo $msg ?>
+        </h1>
+
+        <?php if ($showPattern === 'form') { ?>
+            <form method="POST" action="./">
+                <input type="text" name="user_name" placeholder="Your name">
+                <input type="submit" value="Enter">
+            </form>
+        <?php } else { ?>
+            <a href="./">Back</a>
+        <?php } ?>
     </body>   
 </html>
